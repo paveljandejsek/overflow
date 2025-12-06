4902c4d13149cc385ac2819075da3adbf209d24e
Pavel Jandejsek <pavel.jandejsek@gmail.com>
2025-12-06 18:25:06 +0100

    Initial project setup.

---
+++ src/main/java/com/github/paveljandejsek/overflow/OverflowApplication.java
@@ -0,0 +1,18 @@
+package com.github.paveljandejsek.overflow;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverflowApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverflowApplication.class, args);
+    }
+
+    @Override
+    public void run(java.lang.String... args) throws Exception {
+
+    }
+}
