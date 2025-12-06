3c2dc7eb2488bb76de4c4eadf32befa9c9bbc31a
Pavel Jandejsek <pavel.jandejsek@gmail.com>
2025-12-06 22:59:30 +0100

    Add basic calculation implementation.

--- src/main/java/com/github/paveljandejsek/overflow/OverflowService.java
+++ src/main/java/com/github/paveljandejsek/overflow/OverflowService.java
@@ -13,9 +13,46 @@ public class OverflowService {
         log.atDebug().log("inputFlow: {}, tankCapacities: {}", inputFlow, tankCapacities);
 
 
+        int lastSmallestTankIndex = getLastSmallestTankIndex(tankCapacities);
+        int allFull = (int)Math.floor(getMaxFullTime(tankCapacities, inputFlow));     // incident (2)
 
-        return new OverflowResult(0, 0);
+        List<Integer> lastTankRelevantSizes = tankCapacities.subList(lastSmallestTankIndex, tankCapacities.size());
+        double lastOverflow = getMaxFullTime(lastTankRelevantSizes, inputFlow);
+        int lastOverflowRounded = (int)Math.floor(lastOverflow);   // incident (1)
+
+        return new OverflowResult(lastOverflowRounded, allFull);
     }
 
+    private double getMaxFullTime(List<Integer> tankCapacities, int inputFlow) {
+        // time of previous tank * flow -> what it gets before overflow starts
+        // ((tank index+1)*flow) * time of this tank - overflow of all the previous tanks for the time after
+        // capacity of tank = (time of previous * flow) + ((tank index+1)*flow)* time of this tank
+        // time of this tank = (capacity of this tank + ((tank index+1)*flow)* time of previous tank) / ((tank index+1)*flow)
+
+        double previousTankTime = (double) tankCapacities.getFirst() / inputFlow;
+        double maxFullTime = previousTankTime;
+
+        for (int tankIndex = 1; tankIndex < tankCapacities.size(); tankIndex++) {
+            double tankFillTime = ((double) tankCapacities.get(tankIndex) + (double) tankIndex * inputFlow * previousTankTime) / ((double) (tankIndex + 1) * inputFlow);
+            if (tankFillTime > maxFullTime) maxFullTime = tankFillTime;
+            previousTankTime = tankFillTime;
+        }
+
+        return (long) Math.floor(maxFullTime);
+    }
+
+    private int getLastSmallestTankIndex(List<Integer> tankCapacities) {
+        int smallestIndex = tankCapacities.size() - 1;
+        int smallestSize = tankCapacities.get(smallestIndex);
+        for (int i = tankCapacities.size() - 2; i >= 0; i--) {
+            if (tankCapacities.get(i) < smallestSize) {
+                smallestSize = tankCapacities.get(i);
+                smallestIndex = i;
+            } else {
+                break;
+            }
+        }
+        return smallestIndex;
+    }
 
 }
